775f201a746f21ecc26934b2473365c70cf964ff
EdwinDeve <edwincuentaprincipal@gmail.com>
2023-07-13 13:37:54 -0600

    005

--- src/Componentes/Navbar/navBar.jsx
+++ src/Componentes/Navbar/navBar.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { FiHome } from "react-icons/fi";
 
-import Home from '../../Assets/Images/Escudo_PSI.png'
 
 function navBar() {
     return (
@@ -11,7 +10,6 @@ function navBar() {
             <div className="container-fluid">
                 <Link className="navbar-brand" to={'/'}>
                     <h1><FiHome></FiHome></h1>
-                    {/* <img src={Home} width={58} alt="Escudo de PSI" /> */}
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
